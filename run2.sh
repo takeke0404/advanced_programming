49c94051676f8e598b4964167ce80cd4874b42ad
Takuya HASEGAWA <clf14096@cse.cs.nitech.ac.jp>
2019-05-30 19:46:04 +0900

    level6

--- run2.sh
+++ run2.sh
@@ -59,4 +59,52 @@ then
    done
    echo ""
 fi
+if [ $1 = level6 ]
+then
+   rotation=0
+   echo $bname:
+   convert "${image}" $name
+   for template in tmpproc/rotate0/*.ppm; do
+       echo `basename ${template}`
+       if [ $x = 0 ]
+       then
+	   ./matching $name "${template}" 0 0.5 cwpg
+	   x=1
+       else
+	   ./matching $name "${template}" 0 0.5 wpg
+       fi
+   done
+   rotation=90
+   for template in tmpproc/rotate90/*.ppm; do
+       echo `basename ${template}`
+       if [ $x = 0 ]
+       then
+	   ./matching $name "${template}" 90 0.5 wpg
+	   x=1
+       else
+	   ./matching $name "${template}" 90 0.5 wpg
+       fi
+   done
+   rotation=180
+   for template in tmpproc/rotate180/*.ppm; do
+       echo `basename ${template}`
+       if [ $x = 0 ]
+       then
+	   ./matching $name "${template}" 180 0.3 wpg
+       else
+	   ./matching $name "${template}" 180 0.3 wpg
+       fi
+   done
+   rotation=270
+   for template in tmpproc/rotate270/*.ppm; do
+       echo `basename ${template}`
+       if [ $x = 0 ]
+       then
+	   ./matching $name "${template}" 270 0.3 wpg
+       else
+	   ./matching $name "${template}" 270 0.3 wpg
+       fi
+   done
+   echo ""
+fi
 	 
