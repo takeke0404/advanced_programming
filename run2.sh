fd68cdc8a385f64efe824a66cf4a3439f056f926
Takuya HASEGAWA <clf14096@cse.cs.nitech.ac.jp>
2019-05-31 13:33:50 +0900

    level7途中

--- run2.sh
+++ run2.sh
@@ -108,4 +108,104 @@ then
    done
    echo ""
 fi
-	 
+
+if [ $1 = level7 ]
+then
+    #level1
+   name1="imgproc/level7/level1/"$bname
+   convert "${image}" $name1
+   seq 1 4 | xargs -P4 -n1 sh level1.sh $name1 
+   #level2
+   name2="imgproc/level7/level2/"$bname
+   convert -blur 2x2 "${image}" "${name2}"
+   seq 1 4 | xargs -P4 -n1 sh level2.sh $name2 
+   #level3
+   name3="imgproc/level7/level3/"$bname
+   convert -colors 64 "${image}" "${name3}"
+   seq 1 4 | xargs -P4 -n1 sh level3.sh $name3 
+   #level4
+   name4="imgproc/level7/level4/"$bname
+   convert "${image}" $name4
+   seq 1 4 | xargs -P4 -n1 sh level4.sh $name4 
+   #level5
+   name5="imgproc/level7/level5/"$bname
+   rotation=0
+   echo $bname:
+   convert "${image}" "${name5}"
+   for template in tmpproc/0.5/*.ppm; do
+       echo `basename ${template}`
+       if [ $x = 0 ]
+       then
+	   ./matching $name5 "${template}" rotation 0.5 cwpg
+	   x=1
+       else
+	   ./matching $name5 "${template}" rotation 0.5 wpg
+       fi
+   done
+   for template in tmpproc/1.0/*.ppm; do
+       echo `basename ${template}`
+       if [ $x = 0 ]
+       then
+	   ./matching $name5 "${template}" rotation 0.3 wpg
+       else
+	   ./matching $name5 "${template}" rotation 0.3 wpg
+       fi
+   done
+   for template in tmpproc/2.0/*.ppm; do
+       echo `basename ${template}`
+       if [ $x = 0 ]
+       then
+	   ./matching $name5 "${template}" rotation 0.3 wpg
+       else
+	   ./matching $name5 "${template}" rotation 0.3 wpg
+       fi
+   done
+   echo ""
+   #level6
+   name6="imgproc/level7/level6/"$bname
+   rotation=0
+   echo $bname:
+   convert "${image}" $name6
+   for template in tmpproc/rotate0/*.ppm; do
+       echo `basename ${template}`
+       if [ $x = 0 ]
+       then
+	   ./matching $name6 "${template}" 0 0.5 cwpg
+	   x=1
+       else
+	   ./matching $name6 "${template}" 0 0.5 wpg
+       fi
+   done
+   rotation=90
+   for template in tmpproc/rotate90/*.ppm; do
+       echo `basename ${template}`
+       if [ $x = 0 ]
+       then
+	   ./matching $name6 "${template}" 90 0.5 wpg
+	   x=1
+       else
+	   ./matching $name6 "${template}" 90 0.5 wpg
+       fi
+   done
+   rotation=180
+   for template in tmpproc/rotate180/*.ppm; do
+       echo `basename ${template}`
+       if [ $x = 0 ]
+       then
+	   ./matching $name6 "${template}" 180 0.3 wpg
+       else
+	   ./matching $name6 "${template}" 180 0.3 wpg
+       fi
+   done
+   rotation=270
+   for template in tmpproc/rotate270/*.ppm; do
+       echo `basename ${template}`
+       if [ $x = 0 ]
+       then
+	   ./matching $name6 "${template}" 270 0.3 wpg
+       else
+	   ./matching $name6 "${template}" 270 0.3 wpg
+       fi
+   done
+   echo ""
+fi
