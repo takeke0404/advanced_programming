a8fa90bd0550597cc1d57595da86e8f51950f7d9
takeke0404 <takeke0404@yahoo.co.jp>
2019-06-07 12:03:19 +0900

    levelinf7枚検出できるようにチューニング

--- levelinftmp.sh
+++ levelinftmp.sh
@@ -2,27 +2,47 @@
 name=$1
 base=$2
 rotate=$3
-if [ $4 = 1 ]
+if [ $5 = 1 ]
 then
 	template=$base"/"airgun_women_syufu.ppm
 	echo `basename ${template}`
-	./matching $name ${template} ${rotate} 0.09 pw
+	if [ $4 = 0.5 ]
+	then
+	    ./matching $name ${template} $rotate 0.18 pw
+	else
+	    ./matching $name ${template} $rotate 0.09 pw
+	fi
 fi
-if [ $4 = 2 ]
+if [ $5 = 2 ]
 then
 	template=$base"/"kids_chuunibyou_girl.ppm
 	echo `basename ${template}`
-	./matching $name ${template} ${rotate} 0.09 pw
+	if [ $4 = 0.5 ]
+	then
+	    ./matching $name ${template} $rotate 0.15 pw
+	else
+	    ./matching $name ${template} $rotate 0.09 pw
+	fi
 fi
-if [ $4 = 3 ]
+if [ $5 = 3 ]
 then
 	template=$base"/"mokuzai_hakobu.ppm
 	echo `basename ${template}`
-	./matching $name ${template} ${rotate} 0.07 pw
+	if [ $4 = 0.5 ]
+	then
+	    ./matching $name ${template} $rotate 0.14 pw
+	else
+	    ./matching $name ${template} $rotate 0.07 pw
+	fi
 fi
-if [ $4 = 4 ]
+if [ $5 = 4 ]
 then
 	template=$base"/"ocean_beach_kinzokutanchi.ppm
 	echo `basename ${template}`
-	./matching $name ${template} ${rotate} 0.06 pw
+	if [ $4 = 0.5 ]
+	then
+	    ./matching $name ${template} $rotate 0.15 pw
+	else
+	    ./matching $name ${template} $rotate 0.06 pw
+	fi
 fi
