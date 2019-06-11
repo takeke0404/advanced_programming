80af9ddaaf27a26ff2065787ce3366fa34b78438
takeke0404 <takeke0404@yahoo.co.jp>
2019-06-11 12:22:53 +0900

    answerall.shの実装(全levelの答え合わせ)

--- answerall.sh
+++ answerall.sh
@@ -1,8 +1,8 @@
 #!bin/sh
-sh answer.sh result level1
-sh answer.sh result level2
-sh answer.sh result level3
-sh answer.sh result level4
-sh answer.sh result level5
-sh answer.sh result level6
-sh answer.sh result level7
+sh answer2.sh result level1
+sh answer2.sh result level2
+sh answer2.sh result level3
+sh answer2.sh result level4
+sh answer2.sh result level5
+sh answer2.sh result level6
+sh answer2.sh result level7
